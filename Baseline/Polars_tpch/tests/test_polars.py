6921ed93fb146a2c8b4a9e84237a7baf1cc8444f
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-04 15:21:32 -0600

    Tests(TPCHTimer): Add complex test to ensure no cross-timer communication

--- Baseline/Polars_tpch/tests/test_polars.py
+++ Baseline/Polars_tpch/tests/test_polars.py
@@ -20,3 +20,23 @@ def test_timer_decorator(capsys):
         for i in range(10):
             test_function()
             time.sleep(0.5)
+
+
+def test_timer_multi(capsys):
+    with capsys.disabled():
+
+        @TPCHTimer("test_function_0.1", logging=True)
+        def test_function():
+            time.sleep(0.1)
+
+        @TPCHTimer("test_function_0.3", logging=True)
+        def test_function_two():
+            time.sleep(0.3)
+
+        for i in range(3):
+            test_function()
+            test_function_two()
+            with TPCHTimer("context_manager", logging=True):
+                time.sleep(0.5)
+
+        print(TPCHTimer.times)
