61751e8a80088191ca1ec9eaa10712b18a8f03c5
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-04 13:53:54 -0600

    Feat(TPCHTimer): Implement __enter__ function

--- Baseline/Polars_tpch/src/utils/timerutil.py
+++ Baseline/Polars_tpch/src/utils/timerutil.py
@@ -19,16 +19,18 @@ class TPCHTimer:
             name (str): the process name that you are
             timing
         """
-        pass
+        self.start_time = time.perf_counter()
+        self.name = name
 
     def __exit__(self):
         """Stop the timer within a context manager"""
         pass
 
-    def time(self, func: callable):
-        """Timer function decorator
+    def time(self, func: callable, name: str):
+        """Timer function wrapper
 
         Args:
             func (callable): the function to wrap
+            name (str): the process name to save
         """
         pass
