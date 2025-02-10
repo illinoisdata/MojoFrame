a07c3a83f8d794cd9945824adf55b085a9f389d1
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-09 19:56:23 -0600

    Feat(Query 1): Implement Query 1

---
+++ Baseline/Polars_tpch/src/queries/query1.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+import polars as pl
+
+from utils import utils
+from utils.timerutil import TPCHTimer
+
+Q_NUM = 1
+
+
+def q():
+    VAR1 = datetime(1998, 9, 2)
+    with TPCHTimer("Data load time for Query 1"):
+        q = utils.get_line_item_ds()
+    q_final = (
+        q.filter(pl.col("l_shipdate") <= VAR1)
+        .groupby(["l_returnflag", "l_linestatus"])
+        .agg(
+            [
+                pl.sum("l_quantity").alias("sum_qty"),
+                pl.sum("l_extendedprice").alias("sum_base_price"),
+                (pl.col("l_extendedprice") * (1 - pl.col("l_discount")))
+                .sum()
+                .alias("sum_disc_price"),
+                (
+                    pl.col("l_extendedprice")
+                    * (1.0 - pl.col("l_discount"))
+                    * (1.0 + pl.col("l_tax"))
+                )
+                .sum()
+                .alias("sum_charge"),
+                pl.mean("l_quantity").alias("avg_qty"),
+                pl.mean("l_extendedprice").alias("avg_price"),
+                pl.mean("l_discount").alias("avg_disc"),
+                pl.count().alias("count_order"),
+            ],
+        )
+        .sort(["l_returnflag", "l_linestatus"])
+    )
+
+    utils.run_query(Q_NUM, q_final)
+
+
+if __name__ == "__main__":
+    q()
