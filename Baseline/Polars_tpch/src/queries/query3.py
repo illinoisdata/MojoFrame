e9b52b3b298c8b3f23b01961ee911cbce2cb0818
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-09 20:00:28 -0600

    Feat(Query 3): Implement Query 3

---
+++ Baseline/Polars_tpch/src/queries/query3.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import polars as pl
+
+from utils import utils
+from utils.timerutil import TPCHTimer
+
+Q_NUM = 3
+
+
+def q():
+    var1 = var2 = datetime(1995, 3, 15)
+    var3 = "BUILDING"
+
+    with TPCHTimer(f"Data load time for Query {Q_NUM}"):
+        customer_ds = utils.get_customer_ds()
+        line_item_ds = utils.get_line_item_ds()
+        orders_ds = utils.get_orders_ds()
+
+    q_final = (
+        customer_ds.filter(pl.col("c_mktsegment") == var3)
+        .join(orders_ds, left_on="c_custkey", right_on="o_custkey")
+        .join(line_item_ds, left_on="o_orderkey", right_on="l_orderkey")
+        .filter(pl.col("o_orderdate") < var2)
+        .filter(pl.col("l_shipdate") > var1)
+        .with_column(
+            (pl.col("l_extendedprice") * (1 - pl.col("l_discount"))).alias("revenue")
+        )
+        .groupby(["o_orderkey", "o_orderdate", "o_shippriority"])
+        .agg([pl.sum("revenue")])
+        .select(
+            [
+                pl.col("o_orderkey").alias("l_orderkey"),
+                "revenue",
+                "o_orderdate",
+                "o_shippriority",
+            ]
+        )
+        .sort(by=["revenue", "o_orderdate"], reverse=[True, False])
+    )
+
+    utils.run_query(Q_NUM, q_final)
+
+
+if __name__ == "__main__":
+    q()
