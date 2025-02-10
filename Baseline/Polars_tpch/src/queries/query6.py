43e7681758de07655346bef9ca6af5f70185b138
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-09 20:03:57 -0600

    Feat(Query 6): Implement Query 6

---
+++ Baseline/Polars_tpch/src/queries/query6.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+import polars as pl
+
+from utils import utils
+from utils.timerutil import TPCHTimer
+
+Q_NUM = 6
+
+
+def q():
+    var1 = datetime(1994, 1, 1)
+    var2 = datetime(1995, 1, 1)
+    var3 = 24
+
+    with TPCHTimer(f"Data load time for Query {Q_NUM}"):
+        line_item_ds = utils.get_line_item_ds()
+
+    q_final = (
+        line_item_ds.filter(pl.col("l_shipdate") >= var1)
+        .filter(pl.col("l_shipdate") < var2)
+        .filter((pl.col("l_discount") >= 0.05) & (pl.col("l_discount") <= 0.07))
+        .filter(pl.col("l_quantity") < var3)
+        .with_column(
+            (pl.col("l_extendedprice") * pl.col("l_discount")).alias("revenue")
+        )
+        .select(pl.sum("revenue").alias("revenue"))
+    )
+
+    utils.run_query(Q_NUM, q_final)
+
+
+if __name__ == "__main__":
+    q()
