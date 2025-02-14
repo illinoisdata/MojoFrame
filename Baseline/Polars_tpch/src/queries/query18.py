66d2c1e2cd0ace06f46f28a5bbc13f2cf85c9cb2
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-14 14:30:01 -0600

    Chore: add placeholder code to all of the queries not yet implemented

---
+++ Baseline/Polars_tpch/src/queries/query18.py
@@ -0,0 +1,15 @@
+import polars as pl
+
+from src.utils import utils
+from src.utils.timerutil import TPCHTimer
+
+Q_NUM = 18
+
+
+def q():
+    with TPCHTimer('Data load time for Query {Q_NUM}'):
+        pass
+
+    q_final = pl.LazyFrame()
+
+    utils.run_query(Q_NUM, q_final)
