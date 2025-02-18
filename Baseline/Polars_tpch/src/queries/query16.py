88c7846d0220f6f81d4f0da6c953bf828ed182dc
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-18 14:30:40 -0600

    Feat(Query 16): Implement Query 16

--- Baseline/Polars_tpch/src/queries/query16.py
+++ Baseline/Polars_tpch/src/queries/query16.py
@@ -7,9 +7,34 @@ Q_NUM = 16
 
 
 def q():
-    with TPCHTimer('Data load time for Query {Q_NUM}'):
-        pass
+    with TPCHTimer("Data load time for Query {Q_NUM}"):
+        part_ds = utils.get_part_ds()
+        part_supp_ds = utils.get_part_supp_ds()
+        supp_ds = utils.get_supplier_ds()
 
-    q_final = pl.LazyFrame()
+    supp_ds_filter = (
+        supp_ds.filter(
+            pl.col("s_comment").str.contains("(.*)Customer(.*)Complaints(.*)")
+        ).select("s_suppkey")
+    ).collect()
+
+    part_ds_filter = part_ds.filter(
+        (pl.col("p_brand") != "Brand#45")
+        & (~pl.col("p_type").str.starts_with("MEDIUM POLISHED"))
+        & (pl.col("p_size").is_in([49, 14, 23, 45, 19, 3, 36, 9]))
+    )
+
+    q_final = (
+        part_supp_ds.filter(
+            ~pl.col("ps_suppkey").is_in(pl.lit(supp_ds_filter.get_column("s_suppkey")))
+        )
+        .join(part_ds_filter, left_on="ps_partkey", right_on="p_partkey")
+        .group_by(["p_brand", "p_type", "p_size"])
+        .agg(pl.col("ps_suppkey").n_unique().alias("supplier_cnt"))
+        .sort(
+            ["supplier_cnt", "p_brand", "p_type", "p_size"],
+            descending=[True, False, False, False],
+        )
+    )
 
     utils.run_query(Q_NUM, q_final)
