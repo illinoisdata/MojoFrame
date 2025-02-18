eb1f074d7bad8e606bc7b0057d433e81c5bcf0aa
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-18 14:31:49 -0600

    Feat(Query 17): Implement Query 17

--- Baseline/Polars_tpch/src/queries/query17.py
+++ Baseline/Polars_tpch/src/queries/query17.py
@@ -7,9 +7,24 @@ Q_NUM = 17
 
 
 def q():
-    with TPCHTimer('Data load time for Query {Q_NUM}'):
-        pass
+    with TPCHTimer("Data load time for Query {Q_NUM}"):
+        lineitem_ds = utils.get_line_item_ds()
+        part_ds = utils.get_part_ds()
 
-    q_final = pl.LazyFrame()
+    lineitem_grouped = lineitem_ds.group_by(["l_partkey"]).agg(
+        pl.col("l_quantity").mean().alias("l_quantity_avg")
+    )
+
+    q_final = (
+        part_ds.filter(
+            (pl.col("p_brand") == "Brand#23") & (pl.col("p_container") == "MED BOX")
+        )
+        .join(lineitem_ds, left_on="p_partkey", right_on="l_partkey")
+        .join(lineitem_grouped, left_on="p_partkey", right_on="l_partkey")
+        .filter(pl.col("l_quantity") < 0.2 * pl.col("l_quantity_avg"))
+        .sum()
+        .with_columns((pl.col("l_extendedprice") / 7.0).alias("avg_yearly"))
+        .select(pl.col("avg_yearly"))
+    )
 
     utils.run_query(Q_NUM, q_final)
