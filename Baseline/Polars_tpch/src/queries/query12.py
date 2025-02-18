73a8b2803eabfc0c57d36db9b4e15f514f79700d
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-18 14:25:35 -0600

    Feat(Query 12): Implement Query 12

--- Baseline/Polars_tpch/src/queries/query12.py
+++ Baseline/Polars_tpch/src/queries/query12.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import polars as pl
 
 from src.utils import utils
@@ -7,9 +9,43 @@ Q_NUM = 12
 
 
 def q():
-    with TPCHTimer('Data load time for Query {Q_NUM}'):
-        pass
+    var_ship_mode1 = "MAIL"
+    var_ship_mode2 = "SHIP"
+    var_date = datetime(1994, 1, 1)
+    var_date_interval_1yr = datetime(1995, 1, 1)
 
-    q_final = pl.LazyFrame()
+    with TPCHTimer("Data load time for Query {Q_NUM}"):
+        line_item_ds = utils.get_line_item_ds()
+        orders_ds = utils.get_orders_ds()
+
+    q_final = (
+        orders_ds.join(line_item_ds, left_on="o_orderkey", right_on="l_orderkey")
+        .filter(
+            (pl.col("l_shipmode") == var_ship_mode1)
+            | (pl.col("l_shipmode") == var_ship_mode2)
+        )
+        .filter(pl.col("l_commitdate") < pl.col("l_receiptdate"))
+        .filter(pl.col("l_shipdate") < pl.col("l_commitdate"))
+        .filter(pl.col("l_receiptdate") >= var_date)
+        .filter(pl.col("l_receiptdate") < var_date_interval_1yr)
+        .group_by(["l_shipmode"])
+        .agg(
+            [
+                (
+                    (pl.col("o_orderpriority") == "1-URGENT")
+                    | (pl.col("o_orderpriority") == "2-HIGH")
+                )
+                .sum()
+                .alias("high_line_count"),
+                (
+                    (pl.col("o_orderpriority") != "1-URGENT")
+                    & (pl.col("o_orderpriority") != "2-HIGH")
+                )
+                .sum()
+                .alias("low_line_count"),
+            ]
+        )
+        .sort(by="l_shipmode")
+    )
 
     utils.run_query(Q_NUM, q_final)
