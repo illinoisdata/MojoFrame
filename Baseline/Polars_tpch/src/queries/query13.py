24e65dc6e73a3d087f8b78bbf28acfd7c8285ad8
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-18 14:26:45 -0600

    Feat(Query 13): Implement Query 13

--- Baseline/Polars_tpch/src/queries/query13.py
+++ Baseline/Polars_tpch/src/queries/query13.py
@@ -7,9 +7,24 @@ Q_NUM = 13
 
 
 def q():
-    with TPCHTimer('Data load time for Query {Q_NUM}'):
-        pass
+    with TPCHTimer("Data load time for Query {Q_NUM}"):
+        orders_ds = utils.get_orders_ds()
+        customer_ds = utils.get_customer_ds()
 
-    q_final = pl.LazyFrame()
+    q_final = (
+        customer_ds.join(
+            orders_ds.filter(
+                ~pl.col("o_comment").str.contains("(.*)special(.*)requests(.*)")
+            ),
+            left_on="c_custkey",
+            right_on="o_custkey",
+            how="left",
+        )
+        .group_by(["c_custkey"])
+        .agg(pl.col("o_orderkey").drop_nulls().count().alias("c_count"))
+        .group_by(["c_count"])
+        .agg(pl.count().alias("custdist"))
+        .sort(["custdist", "c_count"], descending=[True, True])
+    )
 
     utils.run_query(Q_NUM, q_final)
