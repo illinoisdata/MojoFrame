d86773d5b3ebf5b875d9213773ce7540ff530739
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-18 14:39:11 -0600

    Feat(Query 22): Implement Query 22

--- Baseline/Polars_tpch/src/queries/query22.py
+++ Baseline/Polars_tpch/src/queries/query22.py
@@ -7,9 +7,39 @@ Q_NUM = 22
 
 
 def q():
-    with TPCHTimer('Data load time for Query {Q_NUM}'):
-        pass
+    var_list = [13, 31, 23, 29, 30, 18, 17]
 
-    q_final = pl.LazyFrame()
+    with TPCHTimer("Data load time for Query {Q_NUM}"):
+        customer_ds = utils.get_customer_ds()
+        orders_ds = utils.get_orders_ds()
+
+    avg_c_acctbal = (
+        customer_ds.filter(
+            (pl.col("c_acctbal") > 0)
+            & (
+                pl.col("c_phone")
+                .str.slice(0, 2)
+                .cast(pl.Int32)
+                .is_in(pl.lit(pl.Series(var_list)))
+            )
+        )
+        .select(pl.col("c_acctbal"))
+        .mean()
+    ).collect()
+
+    q_final = (
+        customer_ds.with_columns(
+            (pl.col("c_phone").str.slice(0, 2).alias("cntrycode").cast(pl.Int32))
+        )
+        .filter(pl.col("cntrycode").is_in(pl.lit(pl.Series(var_list))))
+        .filter(pl.col("c_acctbal") > pl.lit(avg_c_acctbal.get_column("c_acctbal")))
+        .join(orders_ds, left_on=["c_custkey"], right_on=["o_custkey"], how="left")
+        .filter(pl.col("o_orderkey").is_null())
+        .group_by(["cntrycode"])
+        .agg(
+            [pl.count().alias("numcust"), pl.col("c_acctbal").sum().alias("totacctbal")]
+        )
+        .sort("cntrycode")
+    )
 
     utils.run_query(Q_NUM, q_final)
