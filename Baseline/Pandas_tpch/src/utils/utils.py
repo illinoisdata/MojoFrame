b6642fb007906ba4a9bc196620c74783234900fa
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-25 14:14:36 -0600

    Refactor(Pandas): Transfer utils from polars to pandas

---
+++ Baseline/Pandas_tpch/src/utils/utils.py
@@ -0,0 +1,404 @@
+import os
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from src.utils.timerutil import TPCHTimer
+
+# The TPC-H queries to execute, in range [START_QUERY, END_QUERY]
+START_QUERY: int = int(os.environ.get("START_QUERY", 1))
+END_QUERY: int = int(os.environ.get("END_QUERY", 22))
+# The number of times to run each query, values are then averaged
+NUM_TRIALS: int = int(os.environ.get("NUM_TRIALS", 5))
+# Table to track the number of calls for each query to ensure proper logging
+CALL_TABLE: dict[int, int] = {
+    qnum: NUM_TRIALS for qnum in range(START_QUERY, END_QUERY + 1)
+}
+# Whether to include data fetching time in the query duration result
+INCLUDE_IO: bool = os.environ.get("INCLUDE_IO", False) == "True"
+# Whether to record the RAM usage as well
+INCLUDE_RAM: bool = os.environ.get("INCLUDE_RAM", False) == "True"
+if INCLUDE_RAM:
+    # The dictionary with the RAM values
+    RAM_USAGE: dict[str, int] = {}
+# The filetype of the input data
+FILE_TYPE: str = os.environ.get("FILE_TYPE", "csv")
+# Dataset directory
+DATA_DIR: str = os.environ.get("DATA_DIR", "data/")
+# The subdirectory of the data file holding the parquet/feather
+DATA_FILE: str = os.environ.get("DATA_FILE", "tiny_tpch/")
+# Current directory
+CWD: str = os.environ.get("CWD", os.path.dirname(os.path.realpath(__file__)))
+# Whether to print the query results while running
+SHOW_RESULTS: bool = os.environ.get("SHOW_RESULTS", False) == "True"
+# Whether to save the query results to a file
+SAVE_RESULTS: bool = os.environ.get("SAVE_RESULTS", False) == "True"
+# Whether to log the query timings
+LOG_TIMINGS: bool = os.environ.get("LOG_TIMINGS", False) == "True"
+# Whether to test the results of the queries for accuracy
+TEST_RESULTS: bool = os.environ.get("TEST_RESULTS", False) == "True"
+# Absolute path to the data directory
+DATASET_BASE_DIR: str = os.path.join(CWD, DATA_DIR, DATA_FILE)
+# Absolute path to the expected TPC-H query answers
+ANSWERS_BASE_DIR: str = os.path.join(CWD, DATASET_BASE_DIR, "answers/")
+# Output directory
+OUTPUT_BASE_DIR: str = os.path.join(CWD, "outputs/", DATA_FILE)
+if not os.path.exists(OUTPUT_BASE_DIR):
+    os.makedirs(OUTPUT_BASE_DIR, exist_ok=True)
+# Timings CSV output directory
+TIMINGS_FILE: str = os.path.join(CWD, f"{OUTPUT_BASE_DIR}/timings.csv")
+RAM_FILE: str = os.path.join(CWD, f"{OUTPUT_BASE_DIR}/ram_usage.csv")
+# Plots directory
+DEFAULT_PLOTS_DIR: str = os.path.join(CWD, f"{OUTPUT_BASE_DIR}/plots")
+if not os.path.exists(DEFAULT_PLOTS_DIR):
+    os.makedirs(DEFAULT_PLOTS_DIR, exist_ok=True)
+
+
+def fetch_dataset(path: str) -> pd.DataFrame:
+    """Lazily fetch TPC-H dataset found at path
+
+    Args:
+        path (str): the path to the dataset
+
+    Returns:
+        pd.DataFrame: the dataset placed in a pandas
+        dataframe
+    """
+    path = f"{path}.{FILE_TYPE}*"
+    match FILE_TYPE:
+        case "csv":
+            scan = pd.read_csv(path)
+        case "parquet":
+            scan = pd.read_parquet(path)
+        case "feather":
+            scan = pd.read_feather(path)
+        case _:
+            raise IOError(f"File type: {FILE_TYPE} not expected")
+
+    return scan
+
+
+def get_query_answer(query_num: int, base_dir: str = ANSWERS_BASE_DIR) -> pd.DataFrame:
+    """Retrieve the answer to TPC-H query number query_num
+    in the form of a pandas dataframe
+
+    Args:
+        query (int): the TPC-H query number
+        base_dir (str, optional): the answers directory. Defaults to ANSWERS_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: the answer to query in a pandas
+        dataframe
+    """
+    answer_df = pd.read_csv(
+        os.path.join(base_dir, f"{query_num}.csv"),
+        sep=",",
+        header=True,
+    )
+    columns: list[str] = list(answer_df.columns)
+    answer_df = answer_df[[c.strip() for c in columns]]
+
+    return answer_df
+
+
+def test_results(query_num: int, result_df: pd.DataFrame) -> bool:
+    """Test the results of TPC-H query number query_num
+
+    Args:
+        query_num (int): the TPC-H query number to test
+        result_df (pd.DataFrame): the results from running
+        the query
+
+    Returns:
+        bool: whether the passed dataframe is the correct answer
+        or not
+    """
+    with TPCHTimer(f"Query {query_num} result testing"):
+        answer: pd.DataFrame = get_query_answer(query_num)
+        correct = result_df.equals(answer)
+
+        if not correct:
+            print(
+                f"   >> QUERY FAILED << {datetime.now().strftime('(%d-%m-%Y : %H:%M)')} - Query {query_num} did not produce the correct answer."
+            )
+
+        return correct
+
+
+def get_line_item_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the lineitem data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "lineitem"))
+
+
+def get_orders_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the orders data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "orders"))
+
+
+def get_customer_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the customer data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "customer"))
+
+
+def get_region_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the region data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "region"))
+
+
+def get_nation_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the nation data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "nation"))
+
+
+def get_supplier_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the supplier data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "supplier"))
+
+
+def get_part_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the part data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "part"))
+
+
+def get_part_supp_ds(base_dir: str = DATASET_BASE_DIR) -> pd.DataFrame:
+    """Fetch the partsupp data items
+
+    Args:
+        base_dir (str, optional): base dataset directory. Defaults to DATASET_BASE_DIR.
+
+    Returns:
+        pd.DataFrame: pandas dataframe containing the fetched data
+    """
+    return fetch_dataset(os.path.join(base_dir, "partsupp"))
+
+
+def write_row(
+    query_num: str,
+    load: float,
+    exec: float,
+    version: str,
+    success: bool = True,
+) -> None:
+    """Write the timings/RAM results for TPC-H query number query_num
+    to the TIMINGS_FILE/RAM_FILE in a CSV format.
+
+    Args:
+        query_num (str): the TPC-H query number
+        load (float): The data loading time/RAM
+        exec (float): The query execution time/RAM
+        version (str): The pandas version
+        success (bool, optional): Whether the query was a success or not.
+        Defaults to True.
+    """
+    with open(TIMINGS_FILE if not INCLUDE_RAM else RAM_FILE, "a") as f:
+        if f.tell() == 0:
+            f.write("version,query_number,load_time,exec_time,include_io,success\n")
+        f.write(
+            f"{version},{query_num},{load / NUM_TRIALS},{exec / NUM_TRIALS},{INCLUDE_IO},{success}\n"
+        )
+
+
+def run_query(query_num: int, result: pd.DataFrame):
+    """Execute TPC-H query number query_num
+
+    Args:
+        query_num (int): query number (1-22)
+        result (pd.DataFrame): pandas dataframe for processing
+    """
+    CALL_TABLE[query_num] -= 1
+
+    load_time: float = TPCHTimer.times[f"Data load time for Query {query_num}"]
+    exec_time: float = TPCHTimer.times[f"Query {query_num} execution"]
+    if INCLUDE_IO:
+        exec_time += load_time
+        load_time = 0.0
+
+    success: bool = test_results(query_num, result) if TEST_RESULTS else True
+
+    if INCLUDE_RAM and not CALL_TABLE[query_num]:
+        write_row(
+            query_num=str(query_num),
+            load=0,
+            exec=RAM_USAGE[f"Query {query_num} peak RAM"],
+            version=pd.__version__,
+            success=success,
+        )
+    elif LOG_TIMINGS and not CALL_TABLE[query_num]:
+        write_row(
+            query_num=str(query_num),
+            load=load_time,
+            exec=exec_time,
+            version=pd.__version__,
+            success=success,
+        )
+
+    if SHOW_RESULTS:
+        print(f">>> SHOWING RESULTS FOR QUERY {query_num}\n")
+        print(result)
+        print(f">>> END OF RESULTS FOR QUERY {query_num}\n")
+
+    if SAVE_RESULTS:
+        result.to_csv(f"{OUTPUT_BASE_DIR}/query{query_num}.csv")
+
+
+def generate_query_plot():
+    """Generate a bar plot of the query timings
+    and output it to the plots directory inside
+    the output directory
+    """
+    data_load_time: list[float] = []
+    execution_time: list[float] = []
+    for query_num in range(START_QUERY, END_QUERY + 1):
+        if f"Query {query_num} execution" in TPCHTimer.times:
+            execution_time.append(
+                TPCHTimer.times[f"Query {query_num} execution"] / NUM_TRIALS
+            )
+            data_load_time.append(
+                TPCHTimer.times[f"Data load time for Query {query_num}"] / NUM_TRIALS
+            )
+        else:
+            execution_time.append(0)
+            data_load_time.append(0)
+
+    fig, ax = plt.subplots()
+
+    # If INCLUDE_IO is set, concatenate the data load times and query
+    # execution times into one, otherwise have the bar graphs stacked
+    concatenated_times: list[float] = [
+        load + execute for load, execute in zip(data_load_time, execution_time)
+    ]
+    if INCLUDE_IO:
+        ax.bar(range(START_QUERY, END_QUERY + 1), concatenated_times)
+        for i, height in zip(range(START_QUERY, END_QUERY + 1), concatenated_times):
+            ax.text(i, height * 1.05, f"{height:.2f}", ha="center", fontsize=10)
+    else:
+        ax.bar(range(START_QUERY, END_QUERY + 1), data_load_time)
+        ax.bar(
+            range(START_QUERY, END_QUERY + 1),
+            execution_time,
+            bottom=data_load_time,
+        )
+        ax.legend(["Data Loading Time", "Query Execution Time"])
+        for i, height_load, height_exec in zip(
+            range(START_QUERY, END_QUERY + 1), data_load_time, execution_time
+        ):
+            ax.text(i, height_load / 2, f"{height_load:.2f}", ha="center", fontsize=10)
+            ax.text(
+                i,
+                height_load + height_exec / 2,
+                f"{height_exec:.2f}",
+                ha="center",
+                fontsize=10,
+            )
+
+    ax.set_ylim(
+        0,
+        max(concatenated_times) * 1.1,
+    )
+
+    ax.set_xlabel("TPC-H Query")
+    ax.set_ylabel("Execution Time (s)")
+    ax.set_xticks(range(START_QUERY, END_QUERY + 1))
+    ax.tick_params(bottom=False, left=False)
+    ax.spines[["left", "right", "top"]].set_visible(False)
+    ax.spines["bottom"].set_color("lightgray")
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(True, color="lightgray")
+    ax.xaxis.grid(False)
+    ax.set_title("TPC-H Query Execution Time for Pandas")
+    fig.savefig(
+        os.path.join(
+            DEFAULT_PLOTS_DIR,
+            "TIMINGS-" + datetime.now().strftime("%m-%d-%y_%H:%M"),
+        ),
+        dpi=200,
+    )
+
+
+def generate_ram_plot():
+    """Generate a query peak RAM usage
+    and output it to the plots directory
+    """
+    execution_ram: list[float] = []
+    for query_num in range(START_QUERY, END_QUERY + 1):
+        if f"Query {query_num} peak RAM" in RAM_USAGE:
+            execution_ram.append(RAM_USAGE[f"Query {query_num} peak RAM"] / NUM_TRIALS)
+        else:
+            execution_ram.append(0)
+
+    fig, ax = plt.subplots()
+
+    ax.bar(range(START_QUERY, END_QUERY + 1), execution_ram, width=0.2)
+
+    ax.set_ylim(
+        0,
+        max(execution_ram) * 1.1,
+    )
+
+    ax.set_xlabel("TPC-H Query")
+    ax.set_ylabel("Number of Allocations")
+    ax.set_xticks(range(START_QUERY, END_QUERY + 1))
+    ax.tick_params(bottom=False, left=False)
+    ax.spines[["left", "right", "top"]].set_visible(False)
+    ax.spines["bottom"].set_color("lightgray")
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(True, color="lightgray")
+    ax.xaxis.grid(False)
+    ax.set_title("TPC-H Query RAM Usage for Pandas")
+    fig.savefig(
+        os.path.join(
+            DEFAULT_PLOTS_DIR,
+            "RAM-" + datetime.now().strftime("%m-%d-%y_%H:%M"),
+        ),
+        dpi=200,
+    )
