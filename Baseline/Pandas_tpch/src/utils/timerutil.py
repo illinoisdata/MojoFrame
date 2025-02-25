9b07d887c4898236b08d20d6d9a40b0d3db7b39b
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-25 13:42:59 -0600

    Feat(Timer): Add timer util to pandas baseline

---
+++ Baseline/Pandas_tpch/src/utils/timerutil.py
@@ -0,0 +1,61 @@
+import time
+from functools import wraps
+from typing import Any, Callable
+
+
+class TPCHTimer:
+    """Custom class used for timing various stages of
+    the TPC-H benchmarks
+    """
+
+    times: dict[str, float] = {}
+
+    def __init__(self, name: str, logging: bool = False):
+        self.start_time = time.perf_counter()
+        self.name = name
+        self.logging = logging
+
+    def __enter__(self):
+        """Start the timer within a context manager
+
+        Returns:
+            TPCHTimer: The timer created
+        """
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, exc_traceback: Any):
+        """Stop the timer within a context manager"""
+        time_elapsed = time.perf_counter() - self.start_time
+
+        if self.name in TPCHTimer.times:
+            TPCHTimer.times[self.name] += time_elapsed
+        else:
+            TPCHTimer.times[self.name] = time_elapsed
+
+        if self.logging:
+            print(f"{self.name} ran for {time_elapsed:.8f}s")
+
+    def __call__(self, func: Callable[..., Any]):
+        """Timer function wrapper
+
+        Args:
+            func (callable): the function to wrap
+        """
+
+        @wraps(func)
+        def timed_func(*args: Any, **kwargs: Any) -> None:
+            self.start_time = time.perf_counter()
+
+            func(*args, **kwargs)
+
+            time_elapsed = time.perf_counter() - self.start_time
+
+            if self.name in TPCHTimer.times:
+                TPCHTimer.times[self.name] += time_elapsed
+            else:
+                TPCHTimer.times[self.name] = time_elapsed
+
+            if self.logging:
+                print(f"{self.name} ran for {time_elapsed:.8f}s")
+
+        return timed_func
