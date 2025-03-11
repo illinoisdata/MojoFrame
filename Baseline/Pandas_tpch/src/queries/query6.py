635af57d2c4c1a4316cab981de6e1937b5f30dfa
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-03-11 13:14:30 -0500

    feat: implement query 6

--- Baseline/Pandas_tpch/src/queries/query6.py
+++ Baseline/Pandas_tpch/src/queries/query6.py
@@ -7,18 +7,27 @@ Q_NUM = 6
 
 
 def q(INCLUDE_RAM: bool, RAM_USAGE: dict[str, float]):
-    var1 = 757382400.0
-    var2 = 788918400.0
-    var3 = 24.0
+    start_timestamp = 757382400.0
+    end_timestamp = 788918400.0
+    max_quantity = 24
 
     with TPCHTimer(f"Data load time for Query {Q_NUM}"):
-        line_item_ds = utils.get_line_item_ds()
+        lineitem_df = utils.get_line_item_ds()
 
     if INCLUDE_RAM:
         tracemalloc.start()
 
     with TPCHTimer(name=f"Query {Q_NUM} execution", logging=False):
-        ...
+        filtered_df_new = lineitem_df[
+            (lineitem_df["l_shipdate"] >= start_timestamp)
+            & (lineitem_df["l_shipdate"] < end_timestamp)
+            & (lineitem_df["l_discount"] >= (0.05))
+            & (lineitem_df["l_discount"] <= (0.07))
+            & (lineitem_df["l_quantity"] < max_quantity)
+        ]
+        revenue_new = (
+            filtered_df_new["l_extendedprice"] * filtered_df_new["l_discount"]
+        ).sum()
 
     if INCLUDE_RAM:
         _, peak = tracemalloc.get_traced_memory()
@@ -28,3 +37,5 @@ def q(INCLUDE_RAM: bool, RAM_USAGE: dict[str, float]):
             RAM_USAGE[f"Query {Q_NUM} peak RAM"] += peak
         else:
             RAM_USAGE[f"Query {Q_NUM} peak RAM"] = peak
+
+    return revenue_new
